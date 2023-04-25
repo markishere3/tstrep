980ca5784d8cb86580ab24522629fa473ebb1d5d
markishere3 <amegafuru837@gmail.com>
2023-04-25 17:22:47 +0800

    bug fix cause it broke

--- Setup.ps1
+++ Setup.ps1
@@ -172,7 +172,35 @@ Write-HostCenter "Installation Started!"
 if($shouldInstallDrivers -imatch "y")
 {
     Write-HostCenter "GPU Drivers"
-    .\Steps\InstallDrivers.ps1
+    $Bucket = "nvidia-gaming"
+    $KeyPrefix = "windows/latest"
+    $LocalPath = "$home\Desktop\NVIDIA"
+    $Objects = Get-S3Object -BucketName $Bucket -KeyPrefix $KeyPrefix -Region us-east-1
+    
+    Write-Host "Downloading Drivers..."
+    foreach ($Object in $Objects) {
+        $LocalFileName = $Object.Key
+        if ($LocalFileName -ne '' -and $Object.Size -ne 0) {
+            $LocalFilePath = Join-Path $LocalPath $LocalFileName
+            Copy-S3Object -BucketName $Bucket -Key $Object.Key -LocalFile $LocalFilePath -Region us-east-1
+        }
+    }
+    
+    Write-Host "Download Complete!"
+    
+    Write-Host "Installing Drivers..."
+    Start-Process -FilePath "$LocalFilePath" -ArgumentList "/s /n" -Wait 
+    
+    Write-Host "Install complete!"
+    
+    Write-Host "Setting required properties..."
+    New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\nvlddmkm\Global" -Name "vGamingMarketplace" -PropertyType "DWord" -Value "2"
+    
+    Invoke-WebRequest -Uri "https://nvidia-gaming.s3.amazonaws.com/GridSwCert-Archive/GridSwCertWindows_2021_10_2.cert" -OutFile "$Env:PUBLIC\Documents\GridSwCert.txt"
+    
+    Write-Host "Done!"
+    
+    Clear-Host
 
     #This is from Parsec's cloud preperation tool!
     Write-HostCenter "Disabling unnecessary adapters...."
